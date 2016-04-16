6202529e928f9869c6ec18ec5ce87c5584b592a5
Sudheer Kumar <s.k.r1thor@gmail.com>
2016-04-16 19:38:42 +0530

    route middleware to protect API route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -22,7 +22,7 @@ apiRouter.post('/auth', function(req, res){
 					// if user is found and correct password.
 					// create web token
 					var app = require('.././app');
-					var token = jwt.sign(user, app.get('superSecret'), {expiresIn:144045});
+					var token = jwt.sign(user, app.get('superSecret'), {expiresIn:1440});
 
 					//return the information
 					res.json({success:true, message: 'Enjoy the Token..', token: token});
@@ -30,6 +30,47 @@ apiRouter.post('/auth', function(req, res){
 			}
 		}
 	});
+});
+
+// route middeleware to varify token
+apiRouter.use(function(req, res, next){
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	//decode token 
+	if (token) {
+		// verifies secret and exp
+		var app = require('.././app');
+		jwt.verify(token,  app.get('superSecret'), function(err, decoded){
+			if (err) {
+				res.json({success: false, message: 'Failed to authenticate token'});
+			}else{
+				// every thing is good. save to request for use in other route 
+				req.decoded = decoded;
+				next();
+			}
+		});
+	}else{
+		// if there is no token, return a error
+		return res.status(403).send({
+			success: false,
+			message: 'No token provide'
+		});
+	}
+});
+
+apiRouter.get('/alluser', function(req, res){
+	usermodel.find({ }, function(err, users){
+		if (err) {
+			throw err;
+		}else{
+			res.json(users);
+		}
+	});
+});
+
+apiRouter.get('/', function(req, res){
+	res.send('respond with a resource');
 });
 
 module.exports = apiRouter;
