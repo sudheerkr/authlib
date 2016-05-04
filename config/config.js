17d8441a85710fd88975ee93ea191b7c9dd83dd1
Sudheer Kumar <s.k.r1thor@gmail.com>
2016-05-04 08:28:12 +0530

    modify the config file

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	secret: 'authlibpackage',
 	// cerate db link 'mongodb://username:password@host:port/database?options...'
-	database: 'mongodb://authlib:addd@localhost:27017/authlib'
+	database: 'mongodb://authlib:addd@localhost:27017/authlib',
+	sendgrid: {
+		api_key: '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'
+	}
 };
