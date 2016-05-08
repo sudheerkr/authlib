ee8e02350c4bf8055e242de8e7966220c8c95960
Sudheer Kumar <s.k.r1thor@gmail.com>
2016-05-08 09:48:29 +0530

    clustering include in app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var ejs = require('ejs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -19,8 +20,9 @@ var config = require('../config/config');
 var app = express();
 app.set('superSecret', config.secret);
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'jade');
+app.engine('html', ejs.renderFile);
+app.set('views', path.join(__dirname, '../../views'));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+// app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -65,6 +67,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
