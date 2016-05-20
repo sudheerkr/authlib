107e90ba47971289c8c867a82d5ef913a89d298f
Sudheer Kumar <s.k.r1thor@gmail.com>
2016-05-20 22:55:28 +0530

    rename file in build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var gulp_concat = require('gulp-concat');
+var gulp_rename = require('gulp-rename');
+var uglify = require('uglify');
 
 // create task for scripts
 gulp.task('scripts', function(){
 	return gulp.src('public/javascripts/**/*.js')
 							.pipe(gulp_concat('main.js'))
+							.pipe(gulp_rename({suffix: '.min'}))
+							.pipe(uglify())
 							.pipe(gulp.dest('build/js'));
 }); 
 
