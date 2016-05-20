3f0addddbe627068696b408158dacd6456008f71
Sudheer Kumar <s.k.r1thor@gmail.com>
2016-05-20 23:15:11 +0530

    uglify working

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var gulp_concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var gulp_rename = require('gulp-rename');
-var uglify = require('uglify');
 
 // create task for scripts
 gulp.task('scripts', function(){
-	return gulp.src('public/javascripts/**/*.js')
-							.pipe(gulp_concat('main.js'))
+	return gulp.src('public/javascripts/*.js')
+						.pipe(gulp_concat('main.js'))
 							.pipe(gulp_rename({suffix: '.min'}))
-							.pipe(uglify())
+								.pipe(uglify())
 							.pipe(gulp.dest('build/js'));
 }); 
 
