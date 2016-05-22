bf75ac2f946899cd52908e247272d49b2441088e
Sudheer Kumar <s.k.r1thor@gmail.com>
2016-05-22 17:25:39 +0530

    authentication WIP

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,50 +1,56 @@
-(function(){
-	'use strict';
-angular.module("authApp", ['ui.router'])
-	.config(routeConfiguration)
-	.factory('authService', authService);
+(function() {
+    'use strict';
+    angular.module("authApp", ['ui.router'])
+        .config(routeConfiguration);
+        // .factory('authService', authService);
 
-// app configurations
-function routeConfiguration($stateProvider, $urlRouterProvider){
-	$stateProvider
-		.state('index', {
-			url: '/index',
-			templateUrl: 'main.html',
-			controller: 'MainCtrl'
-		})
-		.state('about', {
-			url: '/about',
-			templateUrl: 'about.html',
-			controller: 'AboutCtrl'
-		});
-}
+    // app configurations
+    function routeConfiguration($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('index', {
+                url: '/index',
+                templateUrl: 'main.html',
+                controller: 'MainCtrl'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'about.html',
+                controller: 'AboutCtrl'
+            });
+    }
 
-// authService
-function authService($http, $localStorage){
-	var auth = {
-		Login: Login,
-		Logout: Logout
-	};
-	//return auth
-	return auth;
+    // // authService
+    // function authService($http, $localStorage) {
+    //     var auth = {
+    //         Login: Login,
+    //         Logout: Logout
+    //     };
+    //     //return auth
+    //     return auth;
 
-	function Login(user, password, callback){
-	$http.post('/api/auth', {user: user, password: password}).success(function(data){
-		if (data.token) {
-			$localStorage.currentUser = {username: token.username, token: data.token};
-			$http.defaults.headers.common.Authorization = 'Bearer'+ data.token;
-			callback(true);
-		}
-	}).error(function(err){
-		console.log(err);
-	});
-	}
+    //     function Login(user, password, callback) {
+    //         $http.post('/api/auth', {
+    //             user: user,
+    //             password: password
+    //         }).success(function(data) {
+    //             if (data.token) {
+    //                 $localStorage.currentUser = {
+    //                     username: token.username,
+    //                     token: data.token
+    //                 };
+    //                 $http.defaults.headers.common.Authorization = 'Bearer' + data.token;
+    //                 callback(true);
+    //             }
+    //         }).error(function(err) {
+    //             console.log(err);
+    //         });
+    //     }
 
-	// logout
-	function Logout(){
-		delete $localStorage.token;
-		$http.defaults.headers.common.Authorization = '';
-	}
-	}
-		
+    //     // logout
+    //     function Logout() {
+    //         delete $localStorage.token;
+    //         $http.defaults.headers.common.Authorization = '';
+    //     }
+    // }
+
 })();
---
+++ public/javascripts/services/authService.js
@@ -0,0 +1,69 @@
+(function() {
+    'use strict';
+    angular.module('authApp')
+        .factory('Auth', ['$http', '$rootScope', '$window', 'Session', 'AUTH_EVENTS', authService]);
+
+    // authService
+    function authService($http, $rootScope, $window, Session, AUTH_EVENTS) {
+        var service = {
+            login: loginService,
+            authe: isAuthenticatedService,
+            autho: isAuthorizedService,
+            logOut: logoutService
+        };
+
+        return service;
+
+        // loginService
+        function loginService(user, success, error) {
+            $http.post('/api/auth').success(function(data) {
+                var users = data.users;
+                if (users[user.username]) {
+                    var loginData = users[user.username];
+                    // insert there custom login function
+                    if (user.username == loginData.username && user.password == loginData.password) {
+                        // set the browser session, to avoid relogin on refresh.
+                        $window.sessionStorage["userInfo"] = JSON.stringify(loginData);
+                        // delete login password, for not showing client side
+                        delete loginData.password;
+                        //update current user in session and $rootScope
+                        Session.create(loginData);
+                        // or 
+                        $rootScope.currentUser = loginData;
+                        // fire event on successfull login
+                        $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+                        // success 
+                        success(loginData);
+                    }
+                } else {
+                    // login failed
+                    $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+                    // error
+                    error();
+                }
+            }).error(function(err) {
+
+            });
+        };
+
+        // isAuthenticateService
+        function isAuthenticatedService() {
+            return !!Session.user;
+        };
+
+        // isAuthorizedService: check if the use is authorize to access the next route
+        function isAuthorizedService() {
+            if (!angular.isArray(authorizedRoles)) {
+                authorizedRoles = [authorizedRoles];
+            }
+            return (isAuthenticatedService() && authorizedRoles.indexOf(Session.userRole) !== -1);
+        };
+
+        // logoutService
+        function logoutService() {
+            Session.destroy();
+            $window.sessionStorage.removeItem('userInfo');
+            $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
+        };
+    };
+})();
